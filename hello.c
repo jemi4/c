dc94136b074eadc6c36bcb6a6d83d16a9b07e665
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-01-17 16:59:33 +0530

    hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+int counter;
+    for(counter = 0; counter < n; counter++)
+    {
+        printf("Hello\n");
+    }
+    return 0;
+}

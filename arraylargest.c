6e6b7f5cd8c03e36d3f66d5b3f5be1ff92fbb9e2
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-01-28 18:27:10 +0530

    arraylargest.c

---
+++ arraylargest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int array[50], size, i, lt;
+    printf("\n Enter the size of the array: ");
+    scanf("%d", &size);
+    printf("\n Enter %d elements of  the array: ", size);
+    for (i = 0; i < size; i++)
+        scanf("%d", &array[i]);
+    lt = array[0];
+    for (i = 1; i < size; i++)
+    {
+        if (lt < array[i])
+            lt = array[i];
+    }
+    printf("\n largest element present in the given array is : %d", lt);
+    return 0;
+}

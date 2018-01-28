2e3306b8375091cbbb6678347e4d0d3ed44d2709
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-01-28 18:21:42 +0530

    arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
  int main() 
  {
-    int first, diff, terms, value, sum=0, i;
+    int ft, df, terms, value, sum=0, i;
     printf("Enter the number of terms in AP series\n");
     scanf("%d", &terms);
      printf("Enter first term and common difference of AP series\n");
-    scanf("%d %d", &first, &diff);
-    value = first;
+    scanf("%d %d", &ft, &df);
+    value = ft;
     printf("AP SERIES\n");
     for(i = 0; i < terms; i++) 
     {
         printf("%d ", value);
         sum += value;
-        value = value + diff;
+        value = value + df;
     }
      printf("\nSum of the AP series till %d terms is %d\n", terms, sum);
      getch();

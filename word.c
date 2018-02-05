8a0d585b73eee63be2ed068db044e65828042854
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-05 16:24:49 +0530

    word.c

---
+++ word.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+#include <string.h>
+void main()
+{
+    char str[50];
+    int i=0, word=0;
+    printf("\nEnter Your String: ");
+    gets(str);
+    while (str[i] != '\0')
+    {
+        if (str[i] == ' ')
+        {
+            word++;
+        }
+        else
+        i++;
+    }
+    printf("\nNumber of words: %d", word+1);
+    getch();
+}

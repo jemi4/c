9d2e5ae1f32dab9a3545cd7cc26568ce0a9691e0
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-07 16:55:25 +0530

    largest10.c

---
+++ largest10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+  {
+    int a[10];
+    int i;
+    int largest;
+    printf("Enter ten values:");
+    for (i = 0; i < 10; i++)
+    {
+    scanf("%d", &a[i]);
+    }
+    largest = a[0];
+    for (i = 0; i < 10; i++)
+    {
+if (a[i] > largest) 
+{
+largest = a[i];
+    }
+    }
+    printf("Greatest of ten numbers is %d", largest);
+    return 0;
+  }

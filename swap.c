091b0580a3d5bd5858974fdab7f0f403d5f72a7c
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-07 16:25:37 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,17 @@
+include <stdio.h>
+#include<conio.h>
+void main()
+{     
+      clrscr();
+      double ftNum, sdNum, temporaryVariable;
+      printf("Enter ft num: ");
+      scanf("%lf", &ftNum);
+      printf("Enter sd num: ");
+      scanf("%lf",&sdNum);
+      temporaryVariable = ftNum;
+      firstNumber = sdNum;
+      sdNum = temporaryVariable);
+      printf("\nAfter swapping, ftNum= %.2lf\n", ftNum);
+      printf("After swapping, sdNum = %.2lf", sdNum);
+      getch();
+}

8c2d89935119690c59f9c204dc06426b832b51aa
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-12 16:21:09 +0530

    strcmp.c

---
+++ strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   char p[50], j[50];
+   printf("Enter a string\n");
+   gets(a);
+   printf("Enter a string\n");
+   gets(b);
+ 
+   if (strcmp(a>b))
+      printf("string p is greater");
+   else
+      printf("string j is greater");
+      return 0;
+}

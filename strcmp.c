82d2503e35186c8a5262972fd0723ffddcbe1bdb
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-12 16:37:16 +0530

    Update strcmp.c

--- strcmp.c
+++ strcmp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include<conio.h>
 #include <string.h>
-int main()
+void main()
 {
    char p[50], j[50];
    printf("Enter a string\n");
@@ -12,5 +13,5 @@ int main()
       printf("string p is greater");
    else
       printf("string j is greater");
-      return 0;
+      getch();
 }

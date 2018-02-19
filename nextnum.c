6f5bb68338e30b20834572136f31a4b705afb84e
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-02-19 15:56:34 +0530

    nextnum.c

---
+++ nextnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("enter the number=");
+scanf("%d",&a);
+b=a+1;
+printf("the next number of the given number is=");
+scanf("%d",&b);
+getch();
+}

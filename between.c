c3418da1f182b5d8f56dc1295057d0f56851fb9f
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-02 16:08:25 +0530

    between.c

---
+++ between.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+	int n,l,r;
+  clrscr();
+	scanf("%d%d",&n,&r);
+	if(n>l && n<r)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	    printf("no");
+	}
+	getch();
+}

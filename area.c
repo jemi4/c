bb01b7a0f90b828233e107c3005c8ee3f374a8c1
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-07 16:19:04 +0530

    area.c

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float l,b,area;
+  clrscr();
+	printf("enter the length and breadth");
+	scanf("%f %f",&l,&b);
+	area=l*b;
+	printf("%.5f",area);
+  getch();
+  }

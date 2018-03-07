adc544f1f56cb98ce388e68f4bc2600e32818138
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-07 15:47:25 +0530

    perfectsquare.c

---
+++ perfectsquare.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=10,b=15,c,i=1;
+clrscr();
+c=a*b;
+for(i=0;i<1;i++)
+{
+if(c==(i*i))
+{
+printf("yes\n");
+}
+else
+{
+printf("no\n");
+}
+}
+getch();
+}

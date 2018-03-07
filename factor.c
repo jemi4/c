827cac35607216953b6bba15cab14e238e0a03c4
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-07 15:40:40 +0530

    factor.c

--- factor.c
+++ factor.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
 int i,a,count=0;
+  clrscr();
 scanf("%d",&a);
 for(i=1;i<=a;i++)
 if(a%i==0)
 printf("%d\t",i);
-return 0;
+getch();
 }

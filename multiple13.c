53fb615d5918dc7e8451ff71a3fe291078194e93
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-07 15:44:29 +0530

    multiple13.c

---
+++ multiple13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+int n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}

a2491b4b5145c0adc385c8a674ab3ac68db99e9d
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-12 16:28:30 +0530

    lcm.c

---
+++ lcm.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the values");
+scanf("%d%d",&a,&b);
+c=(a>b)?a:b;
+if((c%a==0)&&(c%b==0))
+{
+printf("the lcm value is%d",c);
+}
+getch();
+}

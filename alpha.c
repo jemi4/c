a03167335035a2871cdaae47bf8e66a2502df0cc
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-12 16:35:35 +0530

    alpha.c

---
+++ alpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char a[500];
+	int i;
+	scanf("%[^\n]s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+	if(a[i]>='0'&&a[i]<='9')
+	printf("%c",a[i]);
+	}
+	return 0;
+}

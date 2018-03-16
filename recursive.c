daa65094caae31e4d2a6a0538c5b43e3005bc069
jemi4 <30549886+jemi4@users.noreply.github.com>
2018-03-16 16:31:44 +0530

    recursive.c

---
+++ recursive.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+ int original,n;
+ clrscr()
+ scanf("%d",&original);
+ n=original;
+ while(n%2==0)
+ {
+ n=n/2;
+ printf("%d\n",n);
+ }
+ printf("%d is the number not div\n",n);
+ {
+ getch();
+}
+}
